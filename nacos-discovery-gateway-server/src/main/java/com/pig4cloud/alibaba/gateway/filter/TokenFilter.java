04fa8cb47b62be7b8d6f5ae44a204e2da9de89fe
zhouwenchen <761075116@qq.com>
2020-07-21 14:30:22 +0800

    添加排序的方式

--- nacos-discovery-gateway-server/src/main/java/com/pig4cloud/alibaba/gateway/filter/TokenFilter.java
+++ nacos-discovery-gateway-server/src/main/java/com/pig4cloud/alibaba/gateway/filter/TokenFilter.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpStatus;
@@ -18,7 +19,7 @@ import reactor.core.publisher.Mono;
  * 自定义 token的filter 的过滤器
  */
 @Component
-public class TokenFilter implements GlobalFilter {
+public class TokenFilter implements GlobalFilter, Ordered {
 
     private final String TOKEN = "token";
     private final String ERROR_MSG = "token is not null";
@@ -34,5 +35,10 @@ public class TokenFilter implements GlobalFilter {
         }
         //  使用其他的过滤器
         return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
     }
 }
